1577c8aa77a5d87c2cad5d71a7fd798c4c75a809
KatjaMN <katjamarianyman@gmail.com>
2022-02-07 10:15:16 +0200

    Tehtävä 8, kesken

--- App.js
+++ App.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, StatusBar, View, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
-import { Button, TextInput } from 'react-native-web';
-import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { Button, TextInput, Text } from 'react-native-web';
+//import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
 
   const [location, setLocation] = useState(null);
-  const [address, setAddress] = useState('');
+  const [addressText, setAddressText] = useState('');
+  const [addressCoordinates, setAddressCoordinates] = useState('');
   const [region, setRegion] = useState('');
 
 
-  const getCoordinates = async () => {
+  const getAddressCoordinates = async () => {
 
     try {
-        const response = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=TING5iJW7YfSuGjqRg3VgVvrBkHnKjGL&location=Washington,DC`);
+        const response = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=TING5iJW7YfSuGjqRg3VgVvrBkHnKjGL&location=${addressText}`);
         const json = await response.json();
         console.log(json.results);
-        setRegion(json.results);
+        setAddressCoordinates(json.results);
     } catch (error) {
         console.error(error);
         Alert.alert('Error', error);
@@ -26,21 +27,27 @@ export default function App() {
 }
 
 
-
-
-const showAddress = () =>{
-  
-  setRegion
-
+const region = {
+  latitude: addressCoordinates.locations.latLng.lat,
+  longitude: addressCoordinates.locations.latLng.lng,
+  latitudeDelta: 0.0322,
+  longitudeDelta: 0.0221
 }
 
 
-
-/*
-const findAddress = () => {
-  const addressCoordinates = address.results;
+const initial = {
+  latitude: 60.200692,
+  longitude: 24.934302,
+  latitudeDelta: 0.0322,
+  longitudeDelta: 0.0221
 }
-*/
+
+
+const coordinates = {
+  latitude: 60.201373,
+  longitude: 24.934041
+};
+
 
   useEffect(() => (async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -55,19 +62,12 @@ const findAddress = () => {
   })(), []);
 
 
-
-
-  const coordinates = {
-    latitude: 60.201373,
-    longitude: 24.934041
-  };
-
-
   return (
     <View style={styles.container}>
        <MapView
         style={styles.map}
         region={ region }
+        initialRegion={ initial }
       >
         <Marker
           coordinate={coordinates}
@@ -75,25 +75,21 @@ const findAddress = () => {
         />
       </MapView>
       
+      <Text>Write an address</Text>
       <TextInput 
       style={styles.input}
       placeholder= {'Write an address'}
-      value={address}
-      onChangeText={text => setAddress(text)} 
+      value={addressText}
+      onChangeText={text => setAddressText(text)} 
        />
 
       <View style={styles.buttonView}>
-        <Button onPress={ getCoordinates } title='Find address' />
+        <Button onPress={ getAddressCoordinates } title='Find address' />
       </View>
 
     </View>
   );
 }
-
-     /*
-   
-      
-*/
 
 const styles = StyleSheet.create({
   container: {
